ea30a87bba9e336bf6b62f9fa12f017b00b877ca
daniash84 <danielle.hess84@gmail.com>
2023-07-21 11:09:18 -0700

    updating js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,23 +2,35 @@ var question = document.querySelector("#question");
 var choices = Array.from(document.querySelectorAll(".choice-text"));
 var progressText = document.querySelector("#progressText");
 var scoreText = document.querySelector("#score");
-var progressBarFull = document.querySelector("#progressBarFull");
 var timer = document.querySelector("#timer");
 var startButton = document.querySelector("#start-btn");
-var submitButton = document.querySelector("#submit-btn");
 var initials = document.querySelector("#initials");
 var highScores = document.querySelector("#high-scores");
 var highScoresList = document.querySelector("#high-scores-list");
 
 var currentQuestion = {};
 var acceptingAnswers = true;
-var score = 0;
-var questionCounter = 0;
 var availableQuestions = [];
-var timeLeft = 60;
 var timerInterval;
 
+startButton.addEventListener("click", startQuiz);
+quizcontainer.classList.add("activeQuiz");
+showQuestions(0);
+questionCounter = 1;
+startTimer(60);
+startTimer(0);
+
+var timeLeft = 60;
+var que_count = 0;
+var que_numb = 1;
+var userScore = 0;
+
+var next_btn.addEventListener("click", nextQuestion);
+
+
+
 var questions = [ {
+    numb: 1,
     question: "What does Var mean?",
     choice1: "Variable", isCorrect: true,
     choice2: "Variance", isCorrect: false,
@@ -27,6 +39,7 @@ var questions = [ {
 },
 
 { 
+    numb: 2,
     question: "How to create a function in JavaScript?",
     choice1: "function = myFunction()", isCorrect: false,
     choice2: "function:myFunction()", isCorrect: false, 
@@ -35,6 +48,7 @@ var questions = [ {
 
 },
 { 
+    numb: 3,
     question: "How do you call a function named myFunction?",
     choice1: "call myFunction()", isCorrect: false,
     choice2: "myFunction()", isCorrect: true,
@@ -42,11 +56,12 @@ var questions = [ {
     choice4: "call.myFunction()", isCorrect: false
 },
 {
-question: "How do you write an IF statement in JavaScript?",
-choice1: "if i = 5", isCorrect: false,
-choice2: "if i == 5 then", isCorrect: false,
-choice3: "if i = 5 then", isCorrect: false,
-choice4: "if (i == 5)", isCorrect: true
+    numb: 4,
+    question: "How do you write an IF statement in JavaScript?",
+    choice1: "if i = 5", isCorrect: false,
+    choice2: "if i == 5 then", isCorrect: false,
+    choice3: "if i = 5 then", isCorrect: false,
+    choice4: "if (i == 5)", isCorrect: true
 },
 
 ]
